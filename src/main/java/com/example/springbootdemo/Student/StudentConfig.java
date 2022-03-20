54d3b0a9b5caaab253cc2f009f927282245b30d8
Isaac Tsui <isaactsui2000@gmail.com>
2022-03-20 19:46:56 +0800

    03/20/22: Saving Students

---
+++ src/main/java/com/example/springbootdemo/Student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.example.springbootdemo.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.MARCH;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student isaac = new Student("Isaac", "isaac@gmail.com", LocalDate.of(2000, MARCH, 21), 21);
+            Student alex = new Student("Alex", "alex@gmail.com", LocalDate.of(2004, MARCH, 21), 21);
+
+            repository.saveAll(List.of(isaac, alex));
+        };
+    };
+}
