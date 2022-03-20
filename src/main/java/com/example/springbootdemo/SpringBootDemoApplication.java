d2b35344b264eb0ada1c1add568de91ac2911d92
Isaac Tsui <isaactsui2000@gmail.com>
2022-03-20 13:26:41 +0800

    03/20/22: Student Class

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,10 @@ public class SpringBootDemoApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return List.of("Hello", "World");
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L, "Isaac", "isaactsui2000@gmail.com", LocalDate.of(2000, Month.MARCH, 21), 21)
+        );
     }
 
 }
