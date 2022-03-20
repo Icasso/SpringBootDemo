32092c337c6384e6e8c0ab4abe30c6c074223250
Isaac Tsui <isaactsui2000@gmail.com>
2022-03-20 13:30:20 +0800

    03/20/22: API Layer

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,28 +1,13 @@
 package com.example.springbootdemo;
 
-import com.example.springbootdemo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
-    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student(1L, "Isaac", "isaactsui2000@gmail.com", LocalDate.of(2000, Month.MARCH, 21), 21)
-        );
     }
 
 }
