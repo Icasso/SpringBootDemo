66a4b39b589bb5b78d09ab2f7f9b1b8ebd918f80
Isaac Tsui <isaactsui2000@gmail.com>
2022-03-20 13:38:18 +0800

    03/20/22: Dependency Injection

--- src/main/java/com/example/springbootdemo/Student/StudentService.java
+++ src/main/java/com/example/springbootdemo/Student/StudentService.java
@@ -1,10 +1,14 @@
 package com.example.springbootdemo.Student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
+
     public List<Student> getStudents() {
         return List.of(
                 new Student(1L, "Isaac", "isaactsui2000@gmail.com", LocalDate.of(2000, Month.MARCH, 21), 21)
